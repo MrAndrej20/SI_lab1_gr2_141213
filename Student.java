818e24bb0e8ffc824129cb2744f769d306842d35
Andrej Sarafimovski <andrejsarafimovski@gmail.com>
2020-03-25 12:10:19 +0100

    student class finished

--- Student.java
+++ Student.java
@@ -3,16 +3,66 @@ class Student {
 	String firstName;
 	String lastName;
 
-	double [] grades;
-	//TODO constructor
+	double[] grades;
 
-	//TODO seters & getters
+	Student(String index, String firstName, String lastName, double[] grades) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.grades = new double[grades.length];
+		for (int i = 0; i < grades.length; i++) {
+			this.grades[i] = grades[i];
+		}
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setGrades(double[] grades) {
+		this.grades = new double[grades.length];
+		for (int i = 0; i < grades.length; i++) {
+			this.grades[i] = grades[i];
+		}
+	}
+
+	public String getIndex() {
+		return this.index;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public double[] getGrades() {
+		return this.grades;
+	}
 
 	public double getAverage() {
-		//TODO
+		double total = 0;
+		for (int i = 0; i < this.grades.length; i++) {
+			total += this.grades[i];
+		}
+		return total / this.grades.length;
 	}
 
 	public int ECTSCredits() {
-		//TODO
+		int credits = 0;
+		for (int i = 0; i < this.grades.length; i++) {
+			credits += this.grades[i] * 6;
+		}
+		return credits;
 	}
 }
